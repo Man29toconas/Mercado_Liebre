78ad0ec47e9887c8129fb65ace4042c9aa6476b6
Man29toconas <toconascruz@gmail.com>
2023-08-12 12:21:28 -0300

    mingo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 3001
 const app = express();
 
 app.use(express.static("public"))
